6ce0a30a149bc1127d442f1c5200eb1abc6fbabd
SathyaRepo <132055343+SathyaRepo@users.noreply.github.com>
2023-09-06 11:23:39 +0530

    Update lolita.sh

--- lolita.sh
+++ lolita.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-# Check if the script is being run as root
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script as root."
-    exit 1
+# Check if Snap is installed
+if ! command -v snap &> /dev/null; then
+    echo "Snap is not installed. Installing Snap..."
+    sudo apt update
+    sudo apt install snapd -y
 fi
 
-# Add Brave's APT repository and key
-curl -fsSLo /usr/share/keyrings/brave-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-archive-keyring.gpg arch=armhf] https://brave-browser-apt-release.s3.brave.com/ stable main" > /etc/apt/sources.list.d/brave-browser.list
+# Install Brave browser using Snap
+echo "Installing Brave browser..."
+sudo snap install brave
 
-# Update package lists
-apt update
-
-# Install Brave browser
-apt install -y brave-browser
-
-# Clean up
-rm /usr/share/keyrings/brave-archive-keyring.gpg
-rm /etc/apt/sources.list.d/brave-browser.list
-
-# Display installation completion message
-echo "Brave browser has been successfully installed on your Raspberry Pi 4."
+# Verify the installation
+if snap list | grep -q brave; then
+    echo "Brave browser has been successfully installed."
+else
+    echo "Failed to install Brave browser."
+fi
